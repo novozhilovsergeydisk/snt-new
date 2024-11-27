9de940dcb791f5784e282560cb20fc9d395ce61b
novozhilovsergeydisk@yandex.ru <novozhilovsergeydisk@yandex.ru>
2024-11-27 21:19:34 +0300

    Change file

---
+++ resources/views/dashboard2.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ru">
+   <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="csrf-token" content="{{ csrf_token() }}">
+      <title>snt</title>
+      <!-- Fonts -->
+      <link rel="preconnect" href="https://fonts.bunny.net">
+      <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+      <!-- Scripts -->
+      <!-- <link rel="modulepreload" href="/build/1O.js" /> -->
+      <link rel="stylesheet" href="/css/build/1e.css" />
+      <link rel="stylesheet" href="/css/table.css" />
+      <script rel="modulepreload" src="/js/build/10.js"></script>
+      <script rel="modulepreload" src="/js/tailwind.js"></script> 
+      <!-- <script src="https://cdn.tailwindcss.com"></script>    -->
+   </head>
+   <body class="font-sans antialiased">
+	<h1>dashboard</h1>
+   </body>
+</html>
